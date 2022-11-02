acaa66a115aade48ed1be345dc4db1ca4ab8b820
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-11-02 10:26:06 -0300

    Add lesson nlw 02

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -17,6 +19,37 @@ async function bootstrap() {
 
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
+
+    return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string()
+    })
+    
+    const { title } = createPoolBody.parse(request.body)
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title: title,
+        code: code
+      }
+    })
+
+    return reply.status(201).send({ code })
+  })
+
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
+
+    return { count }
+  })
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count()
 
     return { count }
   })
